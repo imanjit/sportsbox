88bb10bbc441e9a8ad38467f993d642566f3eaa0
Charlee <Charleslee1904@outlook.com>
2021-05-10 20:22:03 -0700

    added to the handlebars files

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 const home = require("./homeRoute");
 const players = require("./playersRoute");
 const standings =require("./standings.Route");
+const login = require("./loginRoute");
+
 
 router.use('/', home);
 router.use('/', players);
 router.use('/', standings);
-
+router.use('/', login);
 
 module.exports = router;
