d1e04785c77054e0cae1340ff2b675284ad98061
Ant <alopez0218.al@gmail.com>
2021-05-11 22:31:45 -0700

    (Sequelize Connection)

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const routes = require('./controllers/index');
 const exphbs = require('express-handlebars');
-//const sequelize = require('./config/connection');
+const sequelize = require('./config/connection');
 
 
 const app = express();
@@ -19,6 +19,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-app.listen(PORT, () => console.log(`SportsBox is live on ${PORT}`));
-// sequelize.sync({ force: false }).then(() => {
-// });
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`SportsBox is live on ${PORT}`));
+});
